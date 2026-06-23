0ac2620fc37c0fcd9b13c4070036298ba61b9821
kumonlozandes-oss <kumonlozandes@gmail.com>
2026-06-23 10:42:31 -0300

    Fix syntax error in API path string

--- index.js
+++ index.js
@@ -751,7 +751,7 @@ const options = {
   port: 443,
 
   path:
-"/cobranca/v3/cobrancas?dataInicial=2026-04-01&dataFinal=2026-06-30&itensPorPagina=500,
+"/cobranca/v3/cobrancas?dataInicial=2026-04-01&dataFinal=2026-06-30&itensPorPagina=500",
 
   method: "GET",
 
