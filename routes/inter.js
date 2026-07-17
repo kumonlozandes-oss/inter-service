c1491c617b2786dbf0bd90d99d90b18b06749cab
kumonlozandes-oss <kumonlozandes@gmail.com>
2026-07-17 13:59:31 -0300

    Add health check endpoint to inter.js

--- routes/inter.js
+++ routes/inter.js
@@ -1,1 +1,12 @@
-module.exports = {};
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/health", (req, res) => {
+  res.json({
+    modulo: "Inter",
+    status: "OK"
+  });
+});
+
+module.exports = router;
