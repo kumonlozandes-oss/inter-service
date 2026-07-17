c25b044979c6011d2f613bad55feb58e43da4051
kumonlozandes-oss <kumonlozandes@gmail.com>
2026-07-17 13:59:47 -0300

    Add health check endpoint to webhook route

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,1 +1,12 @@
-module.exports = {};
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/health", (req, res) => {
+  res.json({
+    modulo: "Webhook",
+    status: "OK"
+  });
+});
+
+module.exports = router;
