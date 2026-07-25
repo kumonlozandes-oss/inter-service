6754e02b84cea849f8ff7717657892f7a1a27b68
kumonlozandes-oss <kumonlozandes@gmail.com>
2026-07-24 23:25:12 -0300

    Implement FinanceiroRepository with CRUD methods

--- repositories/FinanceiroRepository.js
+++ repositories/FinanceiroRepository.js
@@ -1,1 +1,46 @@
-module.exports = {};
+const supabase = require("./SupabaseRepository");
+
+class FinanceiroRepository {
+
+  async listarTitulosPendentes() {
+
+    const { data, error } = await supabase
+      .from("financeiro_titulos")
+      .select("*")
+      .in("status", [
+        "EMITIDO",
+        "ABERTO",
+        "ATRASADO"
+      ]);
+
+    if (error) throw error;
+
+    return data || [];
+
+  }
+
+  async atualizarTitulo(id, dados) {
+
+    const { error } = await supabase
+      .from("financeiro_titulos")
+      .update(dados)
+      .eq("id", id);
+
+    if (error) throw error;
+
+  }
+
+  async atualizarMensalidade(idMensalidade, dados) {
+
+    const { error } = await supabase
+      .from("mensalidades")
+      .update(dados)
+      .eq("ID_MENSALIDADE", idMensalidade);
+
+    if (error) throw error;
+
+  }
+
+}
+
+module.exports = new FinanceiroRepository();
